17f59b757d436569a7136e5b561da4ba65b0898a
Cristian David Ippolito Carrillo <cristiandavidippolito@hotmail.com>
2021-03-11 20:14:40 -0500

    feat: no uuid format

--- src/modules/users/schemas/create-company-admin.schema.js
+++ src/modules/users/schemas/create-company-admin.schema.js
@@ -1,7 +1,7 @@
 import * as yup from 'yup';
 
 export const createCompanyAdminSchema = yup.object().shape({
-  companyUuid: yup.string().uuid().required(),
+  companyUuid: yup.string().required(),
   email: yup.string().email().required(),
   phone: yup.string().required().min(10).max(10),
   password: yup.string().required().min(5),
