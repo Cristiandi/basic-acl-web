73c349645e4c8f05d1bd3c49bb97b6498e86c17c
Cristian David Ippolito Carrillo <cristiandavidippolito@gmail.com>
2023-07-04 19:38:25 +0000

    fix: fixing GRAPHQL_ENDPOINT config VAR

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 export const API_URL = 'https://api.basic-acl.com/';
-// export const GRAPHQL_ENDPOINT = 'https://api.basic-acl.com/graphql';
-export const GRAPHQL_ENDPOINT = 'http://localhost:8080/graphql';
+export const GRAPHQL_ENDPOINT = 'https://api.basic-acl.com/graphql';
+// export const GRAPHQL_ENDPOINT = 'http://localhost:8080/graphql';
